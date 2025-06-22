b452e6a138038c8b4f0ed469e632844e3486230c
Sunal Kumar Saha <sunalsaha5803@gmail.com>
2025-06-22 10:26:24 +0530

    Fix: Use correct case for Infopage.scss to match import

--- src/components/Infopage.jsx
+++ src/components/Infopage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import './infoPage.scss';
+import './InfoPage.scss';
 
 
 import './Mobile.scss'; // Import mobile styles
